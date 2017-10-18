2d1de0be1815b51e46952198563bb1699ab6546b
Kenric Nguyen 2 <kenric.nguyen@hotmail.com>
2017-10-18 14:58:18 +1100

    Fixed bug in Stats page
    Overwrite the statistics pages

--- AFLTipping Project/AFLTippingSite/Admin/Statistics.aspx.designer.cs
+++ AFLTipping Project/AFLTippingSite/Admin/Statistics.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace M06_Roles.Admin {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
         /// Label3 control.
         /// </summary>
         /// <remarks>
